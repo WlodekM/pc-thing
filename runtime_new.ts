5b5e8481cef26692a37c07630a44f9df7f56a49c
WlodekM <wlodzio.maly@gmail.com>
2025-03-02 11:23:06 +0200

    instruction map thing and wip interrupt clock

--- runtime_new.ts
+++ runtime_new.ts
@@ -4,16 +4,16 @@ type instruction = {function: (this: PC, argv: number[]) => void, args: number}
 
 class Runtime {
     pc: PC = new PC()
-    instructions: instruction[] = []
+    instructions: Record<string, instruction> = {}
     instructionNames: string[] = []
 
     addInstruction(name: string, instruction: instruction) {
         this.instructionNames.push(name)
-        this.instructions.push(instruction)
+        this.instructions[name] = instruction
     }
 
     run(line: number[]) {
-        const instructionId = line.shift()
+        const instructionId = this.pc.instructions[line.shift() ?? -1]
         if (instructionId == undefined || !this.instructions[instructionId])
             throw 'unknown instruction (1)';
         const instruction = this.instructions[instructionId];
@@ -22,7 +22,7 @@ class Runtime {
         try {
             instruction.function.call(this.pc, line)
         } catch (error) {
-            console.error(error, 'at', this.pc.programPointer, this.instructionNames[instructionId])
+            console.error(error, 'at', this.pc.programPointer, instructionId)
         }
     }
 }
@@ -47,23 +47,41 @@ runtime.pc.mem = runtime.pc.mem.toSpliced(65534 / 2 + 1, 0, ...[...iram].reduce<
     return result;
 }, []))
 
-runtime.pc.programPointer = 65534 / 2 + 1
+runtime.pc.programPointer = 65536 / 2
+
+function gotoInterrupt() {
+    if (!runtime.pc.mem[65536 / 2 - 1])
+        return;
+    runtime.pc.returnStack.push(runtime.pc.programPointer);
+    runtime.pc.programPointer = runtime.pc.mem[65536 / 2 - 1]
+}
+
+const interruptInterval = setInterval(gotoInterrupt, 10)
+
+const endInst = (Object.entries(runtime.pc.instructions) as [unknown, string][] as [number, string][])
+    .find(([_, b]: [number, string]) => b == 'end')
+if (!endInst) throw 'where the fuck is the end instruction'
+const endInstId = endInst[0];
+
 
 // let c = 0
 while (
-    runtime.pc.mem[runtime.pc.programPointer] != runtime.instructions.length - 1 &&
+    runtime.pc.mem[runtime.pc.programPointer] != endInstId &&
     runtime.pc.programPointer != 0xFFFF - 1) {
     try {
-        const instruction = runtime.pc.mem[runtime.pc.programPointer]
+        const definition = Object.entries(runtime.pc.instructions).find(([a]) => +a == runtime.pc.mem[runtime.pc.programPointer])
+        if (!definition || !definition[1]) throw 'what the fuck is that'
+        const instruction = definition[1]
         runtime.pc.programPointer ++
+        // console.debug(instruction, runtime.instructions, definition)
         const args = [];
         if (Deno.args.includes('-d'))
-        console.debug(runtime.pc.programPointer, instruction, runtime.instructionNames[instruction], runtime.pc.mem[runtime.pc.programPointer+1])
+        console.debug(runtime.pc.programPointer, instruction, instruction, runtime.pc.mem[runtime.pc.programPointer+1])
         while (args.length < runtime.instructions[instruction].args) {
             args.push(runtime.pc.mem[runtime.pc.programPointer])
             runtime.pc.programPointer++
         }
-        runtime.run([instruction, ...args])
+        runtime.run([+definition[0], ...args])
         // runtime.pc.programPointer++
         // c++
     } catch (error) {
@@ -72,8 +90,10 @@ while (
     }
 }
 
+clearInterval(interruptInterval)
+
 if (Deno.args.includes('-d'))
-    console.debug(Object.fromEntries(runtime.instructions.map((_, i) => [i, runtime.instructionNames[i]])),)
+    console.debug(Object.values(runtime.pc.instructions))
 console.debug('end of execution, dumping ram', runtime.pc.mem)
 Deno.writeFileSync('ram.bin', Uint8Array.from(runtime.pc.mem.map(a => [a & 0x00FF, (a & 0xFF00) >> 8]).flatMap(([a, b]) => [a, b])))
 new Deno.Command('hexdump', {
