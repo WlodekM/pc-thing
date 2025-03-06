316496ebb7952dc79e847fb678f8d77f5daf03c2
WlodekM <wlodzio.maly@gmail.com>
2025-03-06 22:44:33 +0200

    debug info for invalid register

--- lib.ts
+++ lib.ts
@@ -1,5 +1,5 @@
 export function parseReg(arg: number): number {
     if (!'ABC'.split('').includes(String.fromCharCode(arg).toUpperCase()))
-        throw 'invalid register';
+        throw `invalid register "${String.fromCharCode(arg)}"`;
     return 'ABC'.split('').indexOf(String.fromCharCode(arg).toUpperCase())
 }
