1d94ffb133bf48526448fa87752ad6ed87f9cac2
WlodekM <wlodzio.maly@gmail.com>
2025-03-07 17:38:35 +0200

    dbg exit thing

--- instructions/dbg.ts
+++ instructions/dbg.ts
@@ -3,7 +3,11 @@ import { PC } from "../pc.ts";
 export default {
     function(this: PC, [msg]: string[]) {
         console.log(msg, this.mem, this.registers, this.returnFlag, this.returnStack);
-        Deno.stdin.readSync(new Uint8Array(1))
+        const b = new Uint8Array(1);
+        Deno.stdin.readSync(b)
+        if (b[0] == 'c'.charCodeAt(0)) {
+            this.programPointer = 0xFFFF - 1
+        }
     },
     args: 1
 }
