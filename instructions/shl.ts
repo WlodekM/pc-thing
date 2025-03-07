422ad754156157a19e7256c2d0be1512a6c2210b
WlodekM <wlodzio.maly@gmail.com>
2025-03-07 21:02:29 +0200

    shl&shr

---
+++ instructions/shl.ts
@@ -0,0 +1,9 @@
+import { PC } from "../pc.ts";
+
+export default {
+    function(this: PC, [reg, by]: number[]) {
+        const r = this.lib.parseReg(reg);
+        this.registers[r] = this.registers[r] << by
+    },
+    args: 2
+}
