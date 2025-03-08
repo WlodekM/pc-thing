365426e228e343674b802fbc811cab15a436cbf9
WlodekM <wlodzio.maly@gmail.com>
2025-03-08 13:21:15 +0200

    uuuuh

--- build-bootloader.sh
+++ build-bootloader.sh
@@ -1,6 +1,7 @@
 deno -A assembler.ts code/bootloader.a
 deno -A ramgen.ts
-cat iram.bin > bootloader.bin
+cat iram.bin > diskloader.bin
 deno -A assembler.ts code/move-bootloader.a
 deno -A ramgen.ts
-cat bootloader.bin >> iram.bin
+cat iram.bin > bootloader.bin
+cat diskloader.bin >> iram.bin
