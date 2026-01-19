d385e0f7d210b7b169ba41a09095a8c7e101b589
WlodekM <wlodzio.maly@gmail.com>
2026-01-19 20:08:50 +0200

    fix notation for flg being weird

--- latest/instructions/flg.ts
+++ latest/instructions/flg.ts
@@ -4,7 +4,7 @@ export default {
     function(this: PC, [reg1, reg2]: number[]) {
         const r1 = this.lib.parseReg(reg1, this, true);
         const r2 = this.lib.parseReg(reg2, this);
-        this.registers[r2] = this.flagZCN(r1, false)
+        this.registers[r1] = this.flagZCN(r2, false)
     },
     args: 2,
     arg_types: 'rr'
