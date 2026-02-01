0bc79fa867d2a6bc70e83984fa93d2abf07391c8
WlodekM <wlodzio.maly@gmail.com>
2026-02-01 14:14:20 +0200

    uhhghhljgsakfs

--- latest/instructions/cpy.ts
+++ latest/instructions/cpy.ts
@@ -5,9 +5,7 @@ export default {
 		const r1 = this.lib.parseReg(reg1, this);
 		const r2 = this.lib.parseReg(reg2, this);
 		const r3 = this.lib.parseReg(reg3, this);
-		console.log({r1,r2,r3})
 		for (let offset = 0; offset < r3; offset++) {
-			console.log(offset, r1+offset, this.getMem(r2+offset), r2+offset)
 	        this.setMem(r1+offset, this.getMem(r2+offset));
 		}
 	},
